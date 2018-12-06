16ee261c8c506af9d1621b953b28099d6f4d6b08
PontusBjorkgard <39138994+PontusBjorkgard@users.noreply.github.com>
2018-12-06 15:33:13 +0200

    Storage implementering
    I add, remove, update metoderna finns skapas nu ingredients array med storage set( listType.id ).

--- src/pages/ingredients/ingredients.ts
+++ src/pages/ingredients/ingredients.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-ingredients',
@@ -9,25 +10,33 @@ export class IngredientsPage {
 
   listType: any;
   ingredients: Array<{ product: string, quantity: number }>;
+  storage: Storage;
 
-
-  constructor(public navCtrl: NavController, navParams: NavParams) {
+  constructor(public navCtrl: NavController, navParams: NavParams, public store: Storage) {
     this.listType = navParams.data;
     this.ingredients = [];
+    this.storage = store;
+    this.storage.get(this.listType.id).then( (val) => {
+      if (val != null) {
+        this.ingredients = val;
+      }
+
+    });
   }
 
 
   addIngredient( product, quantity) {
 
     let duplicate = false;
-    this.ingredients.forEach( ( ingredient ) => {
-      if (ingredient.product == product.value) {
-        console.log('Finns redan');
-        ingredient.quantity = +ingredient.quantity + +quantity.value;
-        duplicate = true;
-        return;
-      }
-    });
+      this.ingredients.forEach( ( ingredient ) => {
+        if (ingredient.product == product.value) {
+          console.log('Finns redan');
+          ingredient.quantity = +ingredient.quantity + +quantity.value;
+          duplicate = true;
+          return;
+        }
+      });
+
 
     if ( !duplicate ) {
       this.ingredients.push(
@@ -35,6 +44,7 @@ export class IngredientsPage {
       );
     }
 
+    this.storage.set( this.listType.id, this.ingredients );
     product.value = null;
     quantity.value = null;
   }
@@ -42,18 +52,22 @@ export class IngredientsPage {
 
   removeIngredient( ingredient ) {
     this.ingredients.splice( this.ingredients.indexOf(ingredient), 1 );
+    
+    this.storage.set( this.listType.id, this.ingredients );
   }
 
   updateIngredient( ingredient, product, quantity ) {
+
     ingredient.product = product.value;
     ingredient.quantity = quantity.value;
-
     ingredient.edit = false;
+
+    this.storage.set( this.listType.id, this.ingredients );
   }
 
 
   transferIngredients() {
-    
+
   }
 
 }
